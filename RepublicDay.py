d3447aa072862285efdc6722c72177b882022e50
soham biswas <96686822+PentesterSoham@users.noreply.github.com>
2022-01-29 00:10:15 -0800

    Add files via upload

---
+++ RepublicDay.py
@@ -0,0 +1,22 @@
+import time
+from random import randint
+
+for i in range(1,45):
+    print('')
+    
+
+s = ''
+
+for i in range(1,1000):
+    count = randint(1,100)
+    while(count>0):
+        s= s + ' '
+        count -=1 
+
+
+    if(i%10==0):
+        print(s+ 'HAPPY INDEPENDENCE DAY')
+    else:
+        print(s+ '*')
+    s= ''
+    time.sleep(0.07)
